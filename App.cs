0a3092dcea19593afe564d41efca968dd115b415
kumaravel <velk9111@gmail.com>
2025-05-26 17:15:57 +0530

    Initial commit

---
+++ App.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class App
+{
+    private readonly IHttpClientFactory _clientFactory;
+
+    public App(IHttpClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
+    public async Task Run()
+    {
+        var client = _clientFactory.CreateClient("MyClientWithCert");
+        var response = await client.GetAsync("https://postman-echo.com/get");
+
+        Console.WriteLine($"Status: {response.StatusCode}");
+        string content = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"Content: {content}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                services.AddHttpClient("MyClientWithCert")
+                    .ConfigurePrimaryHttpMessageHandler(() =>
+                    {
+                        var handler = new HttpClientHandler();
+
+                        var cert = new X509Certificate2("..//..//shared//cert//testcert.pfx", "Test1234!");
+
+                        handler.ClientCertificates.Add(cert);
+
+                        return handler;
+                    });
+
+                services.AddTransient<App>();
+            })
+            .Build();
+
+        await host.Services.GetRequiredService<App>().Run();
+    }
+}
